902c7dfc6d92dd21d1dcefcfcc62d46f0d3fdd7b
jaehun_park2 <jaehun_park2@tmax.co.kr>
2022-03-14 19:15:51 +0900

    feat(service): 필요없는 코드 제거와 주석 수정

--- pages/login/callback/kakao.tsx
+++ pages/login/callback/kakao.tsx
@@ -3,11 +3,12 @@ import { useEffect } from "react";
 
 const KakaoLoginCallback: NextPage = () => {
   useEffect(() => {
+    // get previous url from local storage
     const previousUrl: string = window.localStorage.getItem(
       "kakao-previous-url",
     ) as string;
-    // send code to server
-
+    // send code to server and wait for the cookie
+    // then set token in cokkie
     // redirect to previousUrl
     window.location.href = previousUrl;
   }, []);
